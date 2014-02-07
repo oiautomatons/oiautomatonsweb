b5336373499386750d23989ce580829ec3f8f072
Connor Belanger <connor.belanger@gmail.com>
2014-02-06 23:39:54 -0700

    Version 3 ---- -Major restructure -Navigation Added -Social Media buttons added

---
+++ Scripts/facebook_like.js
@@ -0,0 +1,9 @@
+		//This function was provided and created by Facebook.
+		(function(d, s, id) {
+  		var js, fjs = d.getElementsByTagName(s)[0];
+  		if (d.getElementById(id)) return;
+  		js = d.createElement(s); js.id = id;
+  		js.src = "http://connect.facebook.net/en_US/all.js#xfbml=1";
+  		fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+		
