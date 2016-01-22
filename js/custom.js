48ac7d6e45895cae9ed589c61f80c5ec9d36d4c6
Connor Belanger <connor.belanger@gmail.com>
2016-01-22 00:14:58 +0000

    Fixed spelling, grammatical, and sponsor issues.

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
+    
     $('.materialboxed').materialbox();
     $('.dropdown-button').dropdown({ hover: true });
     $('.button-collapse').sideNav();
-    $('.pin-wrapper').pushpin({ offset: 10 });
+    $('.pin-wrapper').pushpin({ top: $('nav').height() });
+    
+});
+
+$('#maincontent').bind('DOMSubtreeModified', function(){
+    
+    $('#welcome').fadeTo("slow", 1);
+    $('#teamBanner').delay(500).fadeTo("slow", 1);
+    $('#video').delay(1000).fadeTo("slow", 1);
+    $('#contactInfo').delay(1500).fadeTo("slow", 1);
+    
 });
